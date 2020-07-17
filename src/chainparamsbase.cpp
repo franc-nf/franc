4a2835662a6d195dcbc99f41ef74776fd7d5a4fd
RomyToklio <contact@evaveo.com>
2020-07-17 09:40:33 +0200

    commit

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -36,11 +36,11 @@ const CBaseChainParams& BaseParams()
 std::unique_ptr<CBaseChainParams> CreateBaseChainParams(const std::string& chain)
 {
     if (chain == CBaseChainParams::MAIN)
-        return MakeUnique<CBaseChainParams>("", 48931);
+        return MakeUnique<CBaseChainParams>("", 48941);
     else if (chain == CBaseChainParams::TESTNET)
-        return MakeUnique<CBaseChainParams>("testnet3", 48933);
+        return MakeUnique<CBaseChainParams>("testnet3", 48943);
     else if (chain == CBaseChainParams::REGTEST)
-        return MakeUnique<CBaseChainParams>("regtest", 48935);
+        return MakeUnique<CBaseChainParams>("regtest", 48945);
     else
         throw std::runtime_error(strprintf("%s: Unknown chain %s.", __func__, chain));
 }
